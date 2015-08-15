2478761a1618fd97a97a637fe6955875a2017229
yulin0629 <yulin0629@gmail.com>
2015-08-15 13:56:40 +0800

    Finished selection sort playground

--- selection sort.playground/Contents.swift
+++ selection sort.playground/Contents.swift
@@ -2,3 +2,15 @@
 
 import Cocoa
 
+var array = [3, 6, 9, -8, 11]
+
+for i in 0..<array.count {
+    var min_index = i
+    for j in i+1..<array.count {
+        if (array[j] < array[min_index]) {
+            min_index = j
+        }
+    }
+    swap(&array[i], &array[min_index])
+}
+array
